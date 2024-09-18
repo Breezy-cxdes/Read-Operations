dc1c0f99d6a3acde00f0cfd75085a57adb27ad86
Breezy-cxdes <snowbreezy20@gmail.com>
2024-09-18 18:49:36 +0200

    initial commit

--- main.py
+++ main.py
@@ -17,3 +17,19 @@ print("Looping through the lines...")
 for line in file:
     print(line)
 file.close()
+
+
+# Program to remove lines starting with the prefix 'Coding'
+file1 = open('Codingal.txt', 'r')  # Open the original file in read mode
+file2 = open('CodingalUpdated.txt', 'w')  # Open a new file in write mode
+
+# Reading each line from the original text file
+for line in file1.readlines():
+    # If the line does not start with "Coding", print it and store it in the new file
+    if not line.startswith('Coding'):
+        print(line)  # Optional: prints the line to console
+        file2.write(line)  # Writes the line to the new file
+
+# Close and save both files
+file2.close()
+file1.close()
